d514e58a2c56447800619ac0d1ae50885116a84c
Jamshid Sobirov <sobirovjamshid18@mail.com>
2024-12-19 12:32:41 +0500

    constraint layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "uz.tracker.yandextaskviews"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "uz.tracker.yandextaskviews"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
